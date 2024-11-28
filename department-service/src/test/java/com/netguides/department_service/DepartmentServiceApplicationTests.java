a35c91fe3ebaf718ad3d0838f5f341d49bb63b89
Gauravshinde <gaurangshinde94@gmail.com>
2024-11-28 19:49:32 +0530

    Committing DepartmentServiceApplcationTests.java and EmployeeServiceApplicationTests.java files

---
+++ department-service/src/test/java/com/netguides/department_service/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netguides.department_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
