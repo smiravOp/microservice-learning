3987822def399a86b560892d0e0274f8b2057dbe
Gauravshinde <gaurangshinde94@gmail.com>
2024-12-05 12:09:17 +0530

    Open Feign with SR and API Gateway

---
+++ employee-service/src/main/java/com/netguides/employee_service/dto/APIResponseDto.java
@@ -0,0 +1,15 @@
+package com.netguides.employee_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponseDto {
+    EmployeeDto employeeDto;
+    DepartmentDto departmentDto;
+}
